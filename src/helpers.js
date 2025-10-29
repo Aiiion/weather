4a220c8f2edded9a5aabd7ce923359e2a0ce5640
Alex <alex.soderberg97@gmail.com>
2025-10-29 20:39:28 +0100

    add helper + formatting

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,7 @@
 //translated the epoch value to the weekday of the date
 export const translateEpochDay = (epoch) => {
   let newDate = new Date(epoch * 1000);
-  
+
   switch (newDate.getDay()) {
     case 1:
       return "Monday";
@@ -20,20 +20,35 @@ export const translateEpochDay = (epoch) => {
     default:
       return "error";
   }
-}
+};
 //translates the epoch value to time in hours and minutes
-export const translateEpochTime = (epoch) =>{ 
-  let date = new Date(epoch * 1000); 
+export const translateEpochTime = (epoch) => {
+  let date = new Date(epoch * 1000);
   let hour = date.getHours();
   let minute = date.getMinutes();
-  if(minute < 10){
-    minute = `0${minute}`
-  }if(hour < 10){
-    hour = `0${hour}`
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  if (hour < 10) {
+    hour = `0${hour}`;
   }
   return `${hour}:${minute}`;
-}
+};
 
 export const trimIfPhone = (str) => {
   return window.innerWidth > 768 ? str : str.slice(0, 3);
-}
+};
+
+export const getCurrentPosition = () =>
+  new Promise((resolve, reject) =>
+    navigator.geolocation.getCurrentPosition(
+      (position) =>
+        resolve({
+          lat: position.coords.latitude.toFixed(3),
+          lon: position.coords.longitude.toFixed(3),
+        }),
+      () => {
+        reject("Unable to retrieve your location");
+      }
+    )
+  );
