6d9938ae226b0f5415148b55f6188a58229a0222
Alex <alex.soderberg97@gmail.com>
2026-01-29 00:24:07 +0100

    removes unused import

--- src/components/WarningIcon/WarningIcon.jsx
+++ src/components/WarningIcon/WarningIcon.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./WarningIcon.css";
 
 const WarningIcon = ({ color, size = "1em", className = "", title = "", onClick }) => {
--- src/components/WarningModal/WarningModal.jsx
+++ src/components/WarningModal/WarningModal.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./WarningModal.css";
 
 const WarningModal = ({ open, onClose, children }) => {
